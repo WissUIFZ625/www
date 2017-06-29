d6a02001049ffdf06da00b3f82e9da9451d00f95
Streber <streber@Stefans-MacBook-Pro-3.local>
2017-06-29 13:44:55 +0200

    fix the Bug

--- isGitHere.sh
+++ isGitHere.sh
@@ -1,9 +1,10 @@
+    #!/bin/sh
 
-#!/bin/sh
-# gibt es eine Datei mit "git" im Namen?
-which git
-# $? -> 0 = alles ok. jede andere Zahl = nicht ok (sprich: nicht installiert). -eq = equal
-if [ "$?" -eq "0" ] then
-echo "GIT ist installiert."
-else echo "GIT ist nicht installiert."
-fi
+    # GIT ausführen
+    which git
+    # $? -> 0 = alles ok. jede andere Zahl = nicht ok (sprich: nicht installiert). -eq = equal
+    if [ "$?" -eq "0" ]; then
+    echo "GIT ist installiert.";
+    else
+    echo "GIT ist nicht installiert.";
+    fi
